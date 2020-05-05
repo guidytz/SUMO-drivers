b982a625b248f34f7907fd73b3bb3271ca22cead
Guilherme Dytz dos Santos <guidytz@gmail.com>
2020-05-05 13:32:38 -0300

    Adding script to run multiple simulations

---
+++ run_sim.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# Script to run multiple simulations 5 by 5 until the desired number is reached
+
+helpFunction()
+{
+   echo ""
+   echo "Usage: $0 -n num_sims"
+   echo -e "\t-n Number of simulations to run"
+   echo -e "\t-c Net configuration file"
+   echo -e "\t-s Number of simulation steps"
+   echo -e "\t-w Number of steps for simulation to wait before learning"
+   exit 1
+}
+
+# Get parameters from args
+while getopts "n:c:s:w:" opt
+do
+   case "$opt" in
+      n ) num_sims="$OPTARG" ;;
+      c ) net_file="$OPTARG" ;;
+      s ) steps="$OPTARG" ;;
+      w ) wait_learn="$OPTARG" ;;
+      ? ) helpFunction ;;
+   esac
+done
+
+# Print helpFunction in case parameters are empty
+if [ -z "$num_sims" ] || [ -z "$net_file" ] || [ -z "$steps" ] || [ -z "$wait_learn" ]
+then
+   echo "One or more arguments are empty";
+   helpFunction
+fi
+
+# Begin script in case all parameters are correct
+echo "Running simulations in background"
+for i in $(eval echo "{5..$num_sims..5}")
+do  
+    python3 main.py -c $net_file -s $steps -w $wait_learn > /dev/null 2>&1 &
+    sleep 60 && python3 main.py -c $net_file -s $steps -w $wait_learn > /dev/null 2>&1 &
+    sleep 120 && python3 main.py -c $net_file -s $steps -w $wait_learn > /dev/null 2>&1 &
+    sleep 180 && python3 main.py -c $net_file -s $steps -w $wait_learn > /dev/null 2>&1 &
+    sleep 240 && python3 main.py -c $net_file -s $steps -w $wait_learn > /dev/null 2>&1 &
+    wait
+    echo -e "$i..\c"
+done
+echo "Done"
