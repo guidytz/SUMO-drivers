95fcab61dea733e364556674dccc0f6a7d8c5946
guidytz <guidytz@gmail.com>
2021-07-07 16:48:14 -0300

    Adding list of available actions in choose method

--- sumo_ql/exploration/epsilon_greedy.py
+++ sumo_ql/exploration/epsilon_greedy.py
@@ -1,6 +1,9 @@
-import numpy as np
+"""This module implements the class EpsilonGreedy
+"""
 import random as rd
 from datetime import datetime
+
+MAX_SAMPLE_COUNTER = 20
 
 
 class EpsilonGreedy:
@@ -23,26 +26,52 @@ class EpsilonGreedy:
 
         rd.seed(seed)
 
-    def choose(self, q_table, state, action_space):
+    def choose(self, q_table, state, action_space, available_actions):
         """Method that computes the action choice given a Q-table, a state and an action space.
         If value is higher or equal than current epsilon, it chooses the greedy action (highest value in Q-table).
-        If value is lower than current epsilon, it chooses randomly through possible actions at the given state.
+        If value is lower than current epsilon, it chooses randomly through available actions at the given state.
         The epsilon value decays at a decay rate each time this method is called until it reaches its minimum value.
 
         Args:
             q_table (dict): dictionary containing all the q values for each action in each state available.
             state (str): state id the agent is currently in
             action_space (dict): dictionary containing all action spaces for each state available.
+            available_actions (list): list of available indexes within that state, as not all possible actions for the
+            state will be available (they depend on the link the vehicle is coming from).
 
         Returns:
             int: value (index) of the action chosen
         """
         if rd.random() < self.__epsilon:
-            action = int(action_space[state].sample())
+            counter = 0
+            while counter < MAX_SAMPLE_COUNTER:
+                action = int(action_space[state].sample())
+                if action in available_actions:
+                    break
+                counter += 1
+            if counter >= MAX_SAMPLE_COUNTER:
+                print(f"{available_actions = }")
+                print(f"{state = }")
+                print(f"{action = }")
+                print(f"{action_space[state] = }")
+                raise RuntimeError("Something went wrong in sample")
         else:
-            action = np.argmax(q_table[state])
+            available_values = []
+            for action in range(len(q_table[state])):
+                if action in available_actions:
+                    available_values.append(q_table[state][action])
+            max_value = max(available_values)
+            equal_list = list()
+            for index, value in enumerate(q_table[state]):
+                if value == max_value and index in available_actions:
+                    equal_list.append(index)
 
-        self.__epsilon = max(self.__epsilon*self.__decay, self.__min_epsilon)
+            if len(equal_list) != 1:
+                action = rd.choice(equal_list)
+            else:
+                action = equal_list[0]
+
+        self.__decay_epsilon_value()
 
         return action
 
@@ -50,3 +79,8 @@ class EpsilonGreedy:
         """Method that resets the current epsilon value to its initial one.
         """
         self.__epsilon = self.__initial_epsilon
+
+    def __decay_epsilon_value(self):
+        """Method that performs a decay in epsilon value if possible.
+        """
+        self.__epsilon = max(self.__epsilon*self.__decay, self.__min_epsilon)
--- tests/EpsilonGreedy_tests.py
+++ tests/EpsilonGreedy_tests.py
@@ -1,44 +1,47 @@
 import unittest
-import unittest.mock as mock
-from sumo_ql import exploration
-from sumo_ql.exploration import EpsilonGreedy
 from gym import spaces
+from sumo_ql.exploration import EpsilonGreedy
+
 
 class EpsilonGreedyTest(unittest.TestCase):
-
     def setUp(self) -> None:
-        self.__action_space = {'A0': spaces.Discrete(4), 'A1': spaces.Discrete(3)}
+        self.__action_space = {
+            'A0': spaces.Discrete(4),
+            'A1': spaces.Discrete(3)
+        }
         self.__q_table = {'A0': [2, 7, 3, 6], 'A1': [1, 2, 3]}
-        self.__action_space['A0'].seed(5) # values 2 and 3 are the first values to show up with this seed
-        
+
+        # values 2 and 3 are the first values to show up with this seed
+        self.__action_space['A0'].seed(5)
 
     def test_choose(self):
-        exploration = EpsilonGreedy(0.05, 0.05, 0.99, 1) # 10 and 14 call for random < 0.05 with this seed
+        # 10 and 14 call for random < 0.05 with this seed
+        exploration = EpsilonGreedy(0.05, 0.05, 0.99, 1)
 
         for _ in range(9):
-            action = exploration.choose(self.__q_table, 'A0', self.__action_space)
+            action = exploration.choose(self.__q_table, 'A0', self.__action_space, available_actions=[1, 2, 3])
             self.assertEqual(action, 1)
 
-        action = exploration.choose(self.__q_table, 'A0', self.__action_space)
+        action = exploration.choose(self.__q_table, 'A0', self.__action_space, available_actions=[0, 2, 3])
         self.assertEqual(action, 2)
 
         for _ in range(3):
-            action = exploration.choose(self.__q_table, 'A0', self.__action_space)
+            action = exploration.choose(self.__q_table, 'A0', self.__action_space, available_actions=[0, 1, 2])
             self.assertEqual(action, 1)
-        
-        action = exploration.choose(self.__q_table, 'A0', self.__action_space)
-        self.assertEqual(action, 3)
+
+        action = exploration.choose(self.__q_table, 'A0', self.__action_space, available_actions=[0, 1, 2])
+        self.assertEqual(action, 2)
 
     def test_reset(self):
         exploration = EpsilonGreedy(1.0, 0.25, 0.5, 1)
-
-        action = exploration.choose(self.__q_table, 'A0', self.__action_space)
+        available_actions = [0, 1, 2, 3]
+        action = exploration.choose(self.__q_table, 'A0', self.__action_space, available_actions)
         self.assertEqual(action, 2)
 
-        action = exploration.choose(self.__q_table, 'A0', self.__action_space)
+        action = exploration.choose(self.__q_table, 'A0', self.__action_space, available_actions)
         self.assertEqual(action, 1)
 
         exploration.reset()
 
-        action = exploration.choose(self.__q_table, 'A0', self.__action_space)
+        action = exploration.choose(self.__q_table, 'A0', self.__action_space, available_actions)
         self.assertEqual(action, 3)
