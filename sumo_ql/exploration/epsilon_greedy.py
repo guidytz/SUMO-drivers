e47fb1925840b8f9ade24d7e21b397c5a4971cf5
guidytz <guidytz@gmail.com>
2022-11-02 12:42:31 -0300

    Multiple type fixes in environment and simulation code

--- sumo_ql/exploration/epsilon_greedy.py
+++ sumo_ql/exploration/epsilon_greedy.py
@@ -1,9 +1,9 @@
 import random as rd
 from datetime import datetime
-from typing import Dict, Union
-from typing import List
-from gym import spaces
+
 import numpy as np
+from gym import spaces
+
 
 class EpsilonGreedy:
     """Class responsible for the exploration strategy in action choice.
@@ -18,33 +18,33 @@ class EpsilonGreedy:
     def __init__(self, initial_epsilon: float = 1.0,
                  min_epsilon: float = 0.05,
                  decay: float = 0.99,
-                 seed: int = datetime.now()) -> None:
+                 seed: datetime = datetime.now()) -> None:
         self.__initial_epsilon = initial_epsilon
         self.__epsilon = initial_epsilon
         self.__min_epsilon = min_epsilon
         self.__decay = decay
 
-        rd.seed(seed)
+        rd.seed(seed.timestamp())
 
-    def choose(self, q_table: Dict[str, List[float]] or np.ndarray,
+    def choose(self, q_table: dict[str, list[float]] | np.ndarray,
                state: str,
-               action_space: Dict[str, spaces.Discrete],
-               available_actions: List[int]) -> int or Union[int, int]:
+               action_space: dict[str, spaces.Discrete],
+               available_actions: list[int]) -> int | tuple[int, int]:
         """Method that computes the action choice given a Q-table, a state and an action space.
         If value is higher or equal than current epsilon, it chooses the greedy action (highest value in Q-table).
         If value is lower than current epsilon, it chooses randomly through available actions at the given state.
         The epsilon value decays at a decay rate each time this method is called until it reaches its minimum value.
 
         Args:
-            q_table (Dict[str, List[float]] or np.ndarray): Dictionary conaining all the q values for classical
+            q_table (dict[str, list[float]] or np.ndarray): Dictionary conaining all the q values for classical
             Q-Learning or numpy array containing all non-dominated actions for Pareto Q-Learning.
             state (str): state id the agent is currently in.
-            action_space (Dict[str, spaces.Discrete]): dictionary containing all action spaces for each state available.
-            available_actions (List[int]): list of available indexes within that state, as not all possible actions for
+            action_space (dict[str, spaces.Discrete]): dictionary containing all action spaces for each state available.
+            available_actions (list[int]): list of available indexes within that state, as not all possible actions for
             the state will be available (they depend on the link the vehicle is coming from).
 
         Raises:
-            RuntimeError: the method raises a RuntimeError in three situations: 
+            RuntimeError: the method raises a RuntimeError in three situations:
                 - if no available action is given.
                 - if the method can't sample any of the given actions.
                 - if class type of 'Q-table' is unkown.
@@ -62,27 +62,23 @@ class EpsilonGreedy:
                 raise RuntimeError("Couldn't take any action, no available actions given.") from IndexError
             if action not in range(action_space[state].n):
                 raise RuntimeError("Available action not in state's action space.")
-        elif isinstance(q_table, dict):
-            action = self.__choose_dict(q_table, state, available_actions)
-        elif isinstance(q_table, np.ndarray):
-            action, chosen_obj = self.__choose_array(q_table, available_actions)
-        else:
-            raise RuntimeError("Q-table is istance of unknown class.")
+        match q_table:
+            case dict():
+                return self.__choose_dict(q_table, state, available_actions)
+            case np.ndarray(_):
+                return self.__choose_array(q_table, available_actions)
+            case _:
+                raise RuntimeError("Q-table is istance of unknown class.")
 
-        if isinstance(q_table, np.ndarray):
-            return action, chosen_obj
-        else:
-            return action
-
-    def __choose_dict(self, q_table: Dict[str, List[float]],
-                            state: str,
-                            available_actions: List[int]) -> int:
+    def __choose_dict(self, q_table: dict[str, list[float]],
+                      state: str,
+                      available_actions: list[int]) -> int:
         """Method that chooses an action if the Q table given is a dictionary.
 
         Args:
-            q_table (Dict[str, List[float]]): Dictionary conaining all the q values.
+            q_table (dict[str, list[float]]): Dictionary conaining all the q values.
             state (str): state id the agent is currently in.
-            available_actions (List[int]): list of available indexes within that state, as not all possible actions for
+            available_actions (list[int]): list of available indexes within that state, as not all possible actions for
             the state will be available (they depend on the link the vehicle is coming from).
 
         Returns:
@@ -94,12 +90,12 @@ class EpsilonGreedy:
         return rd.choice(equal_list)
 
     def __choose_array(self, q_set: np.ndarray,
-                             available_actions: List[int]) -> Union[int, int]:
+                       available_actions: list[int]) -> tuple[int, int]:
         """Method that chooses an action if the Q table given is a numpy array.
 
         Args:
             q_set (np.ndarray): numpy array containing all non dominated actions for the current state.
-            available_actions (List[int]): list of available indexes within that state, as not all possible actions for
+            available_actions (list[int]): list of available indexes within that state, as not all possible actions for
             the state will be available (they depend on the link the vehicle is coming from).
 
         Raises:
