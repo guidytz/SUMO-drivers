d4cf6b16e72514edf035693daf7dd1829c571adf
guidytz <guidytz@gmail.com>
2022-12-19 15:31:04 -0300

    Adding function that creates parser

--- tests.py
+++ tests.py
@@ -1,33 +1,11 @@
-import argparse
 import sys
 
-from script_configs import CustomParser
-from script_configs.configs import (NonLearnerConfig, PQLConfig, QLConfig,
-                                    add_fields)
+from script_configs import create_parser
+from script_configs.configs import NonLearnerConfig, PQLConfig, QLConfig
 
 
 def main(command_line=None):
-    parser = CustomParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter, fromfile_prefix_chars="@")
-    subparsers = parser.add_subparsers(dest="command", help="Possible agent types")
-
-    ql_base_config = QLConfig()
-    ql = subparsers.add_parser(ql_base_config.name, help=ql_base_config.__doc__,
-                               formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    ql = add_fields(ql, ql_base_config)
-    ql.set_defaults(func=QLConfig.from_namespace)
-
-    nl_base_config = NonLearnerConfig()
-    nl = subparsers.add_parser(nl_base_config.name, help=nl_base_config.__doc__,
-                               formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    nl = add_fields(nl, nl_base_config)
-    nl.set_defaults(func=NonLearnerConfig.from_namespace)
-
-    pql_base_config = PQLConfig()
-    pql = subparsers.add_parser(pql_base_config.name, help=pql_base_config.__doc__,
-                                formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    pql = add_fields(pql, pql_base_config)
-    pql.set_defaults(func=PQLConfig.from_namespace)
-
+    parser = create_parser()
     options = parser.parse_args(command_line)
 
     try:
